579264aacd7cc929aae83707f35363c7130294d8
SR PROGRAMMER <uptimisedcorporationph@gmail.com>
2023-02-02 13:35:55 +0800

    test

---
+++ b4hidden.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Welcome to Uptimised Corp</title>
+  <style>
+    body {
+      margin: 0;
+      padding: 0;
+      height: 100vh !important;
+      justify-content: center;
+      align-items: center;
+      display: flex;
+    }
+
+    .href {
+      font-family: 'Roboto', sans-serif;
+      font-size: 16px;
+      font-weight: bold;
+      background: #1E90FF;
+      width: 200px;
+      padding: 15px;
+      text-align:center;
+      text-decoration: none;
+      text-transform: uppercase;
+      color:#fff;
+      border-radius: 5px;
+      cursor: pointer;
+      margin: 0 15px;
+    }
+  </style>
+</head>
+<body>
+  <a href="login.php" class="href">Book As A Reseller</a>
+  <a href="hiddenhaven/index.php" class="href">Continue As Guest</a>
+</body>
+</html>
