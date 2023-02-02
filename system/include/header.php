0ab9593e56f0103858045f2272423261cffbbf3f
SR PROGRAMMER <uptimisedcorporationph@gmail.com>
2023-02-02 13:50:25 +0800

    test

--- system/include/header.php
+++ system/include/header.php
@@ -2,8 +2,7 @@
 
   session_start();
   include './function.php';
-  // $connect = mysqli_connect('localhost', 'u708090748_uptimised', '@User2022', 'u708090748_uptimisedph'); 
-  $connect = mysqli_connect('localhost', 'root', '', 'uptimisedph');
+  $connect = mysqli_connect('localhost', 'u817058626_uptimisedph', 'Uptimised2022', 'u817058626_uptimisedph'); 
 
   date_default_timezone_set('Asia/Manila');
   $araw_ngayon = date('m-d-Y');
